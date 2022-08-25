df89ee7011071133b5141b3e383d07bd7511dad7
juan.vicente.manyanas@seteuropa.com <AOR8gKctDMx8mKMohS2w>
2022-08-25 15:05:09 +0200

    hasta clase 59

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder: 1400
+  }
+
+  cambiarNombre( event: any ){
+    console.log(event);
+  }
+
   agregar(  ) {
-    console.log("Agregando!...");
+    console.log( this.nuevo );
   }
 
 }
